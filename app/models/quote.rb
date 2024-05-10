29c94f2334fe47fa8958c8c7444aa91f9724b473
devmetal <devmetal91@gmail.com>
2024-05-10 15:24:56 +0200

    add action cable with turbo stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
+# Quote model
 class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit lambda {
+                        broadcast_prepend_to 'quotes',
+                                             partial: 'quotes/quote',
+                                             locals: { quote: self },
+                                             target: 'quotes'
+                      }
 end

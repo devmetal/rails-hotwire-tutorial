dd1854a5c9f523123f779e8b24ce60e04931cdb0
devmetal <devmetal91@gmail.com>
2024-05-12 17:31:04 +0200

    add line items

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :quotes do
-    resources :line_item_dates, except: %i[index show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
+    end
   end
 end
